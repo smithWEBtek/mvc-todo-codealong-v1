b0ed89b8ad7d9ef6626e1c2b75725466bf0228a7
smithWEBtek <brad@smithwebtek.com>
2016-11-18 07:10:09 -0500

    refactor as of 1910 on video

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -7,4 +7,14 @@ module ItemsHelper
   def li_checked_for_item(item)
     true if item.complete?
   end
+
+  def li_for_item(item)
+    content_tag_for :li, item, class: li_class_for_item(item) do
+    # what goes here?
+    # return a string value
+    # string value
+    end
+  end
+
+
 end
